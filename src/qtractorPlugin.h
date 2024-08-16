29485e59357818df6bae39593c5fce9cc4ef321d
windowsrefund <mtf8>
2024-08-16 07:33:12 -0400

    Revert "- VST3: On updating host parameters; only save and load custom"
    This reverts commit 84e998e4496948db911ca9b3e058034eff76a523.

--- src/qtractorPlugin.h
+++ src/qtractorPlugin.h
@@ -725,11 +725,6 @@ public:
 	const QString& name() const
 		{ return m_subject.name(); }
 
-	// Parameter enablement methods.
-	virtual void setValueEnabled(bool /*bEnabled*/) {}
-	virtual bool isValueEnabled() const
-		{ return true; }
-
 	// Parameter range hints predicate methods.
 	virtual bool isBoundedBelow() const = 0;
 	virtual bool isBoundedAbove() const = 0;
@@ -772,7 +767,7 @@ public:
 	void updateValue(float fValue, bool bUpdate);
 
 	// Reset-to-default method.
-	void reset() { setValue(defaultValue(), isValueEnabled()); }
+	void reset() { setValue(defaultValue(), true); }
 
 	// Direct parameter subject value.
 	qtractorSubject *subject() { return &m_subject; }
